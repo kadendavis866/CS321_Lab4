ecd61ec08e6123d28823be8a5de6f0882939e69e
Kaden Davis <kadendavis866@u.boisestate.edu>
2022-04-19 12:55:03 -0600

    Add source files

---
+++ src/GeneBankCreateBTree.java
@@ -0,0 +1,5 @@
+public class GeneBankCreateBTree {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/GeneBankSearch.java
@@ -0,0 +1,5 @@
+public class GeneBankSearch {
+    public static void main(String[] args) {
+
+    }
+}
