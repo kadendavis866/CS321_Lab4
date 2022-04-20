ecd418a2211b59114bf8aff880ec87031c9c1035
Kaden Davis <kadendavis866@u.boisestate.edu>
2022-04-20 15:29:14 -0600

    add BTree.splitChild method

--- src/BTree.java
+++ src/BTree.java
@@ -1,32 +1,63 @@
 public class BTree {
 
+    private final int t;
     private BTreeNode root;
-    private int t;
 
-    public BTree() {
-        root = allocateNode();
-    }
-
-    public BTreeNode allocateNode() {//To be changed
-        return new BTreeNode(t, true);
+    public BTree(int t) {
+        this.t = t;
     }
 
     public void insert(TreeObject k) {
-        BTreeNode r = root;
-        if (r.n == (2 * t - 1)) {
-            BTreeNode s = allocateNode();
-            root = s;
-            s.leaf = false;
-            s.n = 0;
-            s.children[0] = r;
-            splitChild(s, 1);
-            insertNonFull(s, k);
+        if (root == null) {
+            root = new BTreeNode(t, true);
+            root.node[0] = k;
+            root.n = 1;
         } else {
-            insertNonFull(r, k);
+            BTreeNode r = root;
+            if (r.n == (2 * t - 1)) {
+                BTreeNode s = new BTreeNode(t, false);
+                root = s;
+                s.children[0] = r;
+                splitChild(s, 0);
+                insertNonFull(s, k);
+            } else {
+                insertNonFull(r, k);
+            }
         }
     }
 
+    /**
+     * @param nonFull   node containing child to be split
+     * @param fullChild index of node to be split
+     */
     public void splitChild(BTreeNode nonFull, int fullChild) {
+        BTreeNode fullNode = nonFull.children[fullChild];
+
+        // Create a new node
+        BTreeNode newNode = new BTreeNode(t, fullNode.leaf);
+        newNode.n = t - 1;
+
+        // Copy the last (t-1) keys of fullNode to newNode
+        System.arraycopy(fullNode.node, t, newNode.node, 0, t - 1);
+
+        // Copy the last t children of fullNode to newNode
+        if (!fullNode.leaf) {
+            System.arraycopy(fullNode.children, t, newNode.children, 0, t);
+        }
+
+        // update fullNode.n
+        fullNode.n = t - 1;
+
+        // add newNode to parent
+        System.arraycopy(nonFull.children, fullChild + 1, nonFull.children, fullChild + 2, nonFull.n - fullChild);
+        nonFull.children[fullChild + 1] = newNode;
+
+        // add middle key of fullNode to parent
+        System.arraycopy(nonFull.node, fullChild, nonFull.node, fullChild + 1, nonFull.n - fullChild);
+        nonFull.node[fullChild] = fullNode.node[t - 1];
+
+        // update nonFull.n
+        nonFull.n++;
     }
 
     public void insertNonFull(BTreeNode nonFull, TreeObject key) {
